ce30a2594ba771a4b1ea1366b0689a2bd9c62ccf
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-03-10 23:56:33 +0900

    firebase 세팅

--- ios/anabada/AppDelegate.m
+++ ios/anabada/AppDelegate.m
@@ -1,3 +1,4 @@
+#import <Firebase.h>
 #import "AppDelegate.h"
 
 #import <React/RCTBridge.h>
@@ -29,6 +30,10 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  // Add me --- \/
+  [FIRApp configure];
+  // Add me --- /\
+
 #if defined(FB_SONARKIT_ENABLED) && __has_include(<FlipperKit/FlipperClient.h>)
   InitializeFlipper(application);
 #endif
