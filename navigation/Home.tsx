c41af7f3ff578b2640a619c5d48f7442e708df95
roddyisthebest <bsy17171@naver.com>
2022-03-18 12:59:34 +0900

    [esLint 적용] 알람, 등록스크린 구현

--- navigation/Home.tsx
+++ navigation/Home.tsx
@@ -1,57 +1,24 @@
-import React from "react";
+import React from 'react';
 
-import { Text, View } from "react-native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import styled from "styled-components/native";
-
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useNavigation } from "@react-navigation/native";
-
-import Main from "./HomeStack/Main";
-
-import { Ionicons } from "@expo/vector-icons";
-
-import { Badge } from "react-native-elements";
+import Main from './HomeStack/Main';
 
 const Stack = createNativeStackNavigator();
 
-const Btn = styled.TouchableOpacity``;
-
 export default function Home() {
-  const navigation = useNavigation();
-
   return (
     <Stack.Navigator
-      initialRouteName="Home"
       screenOptions={{
         headerBackTitleVisible: false,
-        title: "",
-        headerRight: () => (
-          <Btn
-            onPress={() => {
-              navigation.navigate("Alarm", { screen: "Main" });
-            }}
-          >
-            <Text>
-              <View>
-                <Ionicons size={30} name="notifications" />
+        title: '',
 
-                <Badge
-                  status="error"
-                  value={10}
-                  containerStyle={{ position: "absolute", top: -5, left: 15 }}
-                />
-              </View>
-            </Text>
-          </Btn>
-        ),
-        headerShadowVisible: false,
         contentStyle: {
-          backgroundColor: "white",
+          backgroundColor: 'white',
         },
       }}
     >
-      <Stack.Screen name="Home" component={Main} />
+      <Stack.Screen name="Main" component={Main} />
     </Stack.Navigator>
   );
 }
---
+++ navigation/ItemStack/Enroll.tsx
@@ -0,0 +1,139 @@
+import { useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+
+import { Text, View } from 'react-native';
+import Checkbox from 'expo-checkbox';
+
+import styled from 'styled-components/native';
+
+import { Ionicons } from '@expo/vector-icons';
+
+import Slide from './components/Slide';
+
+const Container = styled.ScrollView`
+  flex: 1;
+`;
+
+const Btn = styled.TouchableOpacity``;
+
+const Inputs = styled.View`
+  padding: 0 25px;
+`;
+
+const InputTitle = styled.TextInput.attrs({
+  placeholderTextColor: '#626262',
+})`
+  background-color: #f2f2f2;
+  height: 50px;
+  width: 100%;
+  border-radius: 10px;
+  margin-bottom: 15px;
+  padding: 10px 15px;
+`;
+
+const InputContent = styled(InputTitle)`
+  height: 150px;
+  padding-top: 15px;
+  margin: 0;
+`;
+
+const InputColumn = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  height: 40px;
+  align-items: center;
+  margin: 15px 0;
+`;
+
+const InputMoney = styled(InputTitle)`
+  height: 35px;
+  width: 100px;
+  margin: 0;
+`;
+
+const ImageText = styled.Text`
+  color: #626262;
+  font-weight: 300;
+  font-size: 20px;
+`;
+
+const StatusText = styled.Text`
+  color: black;
+  font-size: 20px;
+`;
+
+const AgreeBtn = styled.TouchableOpacity`
+  width: 100%;
+  height: 50px;
+  padding: 5px;
+  border-radius: 15px;
+  background-color: #e94057;
+  color: white;
+  justify-content: center;
+  align-items: center;
+`;
+
+function Enroll({
+  navigation: { setOptions },
+}: {
+  navigation: { setOptions: Function };
+}) {
+  const navigation = useNavigation();
+  const [isChecked, setChecked] = useState(false);
+
+  useEffect(() => {
+    setOptions({
+      headerBackTitleVisible: false,
+      headerShadowVisible: false,
+      title: '',
+      headerLeft: () => (
+        <Btn
+          onPress={() => {
+            navigation.goBack();
+          }}
+        >
+          <Text>
+            <Ionicons size={30} name="chevron-back-outline" />
+          </Text>
+        </Btn>
+      ),
+      headerRight: () => null,
+    });
+  }, []);
+
+  return (
+    <Container>
+      <Slide />
+      <Inputs>
+        <InputTitle placeholder="제품명" />
+        <InputContent placeholder="설명" multiline numberOfLines={10} />
+        <InputColumn>
+          <ImageText>보증금</ImageText>
+          <InputMoney />
+        </InputColumn>
+        <StatusText>
+          <Ionicons size={20} name="warning" color="#ffe222" />
+          계좌번호 0000-000-00000에 계좌이체 하셔야 등록이 완료됩니다.
+        </StatusText>
+        <InputColumn>
+          <ImageText>계약명세서 (더보기)</ImageText>
+          <View style={{ flexDirection: 'row' }}>
+            <ImageText>동의</ImageText>
+            <Checkbox
+              style={{ marginLeft: 10 }}
+              value={isChecked}
+              onValueChange={setChecked}
+            />
+          </View>
+        </InputColumn>
+        <AgreeBtn>
+          <Text style={{ color: 'white', fontWeight: '600', fontSize: 20 }}>
+            등록완료
+          </Text>
+        </AgreeBtn>
+      </Inputs>
+    </Container>
+  );
+}
+
+export default Enroll;
