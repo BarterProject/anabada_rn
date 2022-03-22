3478ad2e84f7d4a37d3b0c1684ca9b9b65bc69ab
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-03-22 17:06:31 +0900

    [구현] Slice, App Slice - setIdForSigningUp, setPasswordForSigingUp, setPhoneAuthChecked, setAddressinfo App -  Provider

--- slice.ts
+++ slice.ts
@@ -1,27 +1,82 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+export type stateProps= {
+  name: string,
+  initialsState: initialStateProps
+}
+
+export type initialStateProps ={
+    signUpField: {
+      id: string,
+      password: string,
+      isPhoneAuthChecked: boolean,
+      addressinfo: {
+        zonecode: string,
+        address: string,
+        addressDetail: string,
+      },
+    },
+}
+
 const { actions, reducer } = createSlice({
-  name: 'message',
+  name: 'application',
   initialState: {
-    signUp: {
+    signUpField: {
+      id: '',
+      password: '',
       isPhoneAuthChecked: false,
       addressinfo: {
         zonecode: '',
         address: '',
         addressDetail: '',
       },
-
     },
   },
   reducers: {
     setTest: (state, { payload }: PayloadAction<string>) => ({
       ...state,
     }),
+    setIdForSigningUp: (state, { payload: id }: PayloadAction<string>) => ({
+      ...state,
+      signUpField: {
+        ...state.signUpField,
+        id,
+      },
+    }),
+    setPasswordForSigingUp: (state, { payload: password }: PayloadAction<string>) => ({
+      ...state,
+      signUpField: {
+        ...state.signUpField,
+        password,
+      },
+    }),
+    setPhoneAuthChecked: (state) => ({
+      ...state,
+      signUpField: {
+        ...state.signUpField,
+        isPhoneAuthChecked: true,
+      },
+    }),
+    setAddressinfo: (state, { payload: { zonecode, address } }) => ({
+      ...state,
+      signUpField: {
+        ...state.signUpField,
+        addressinfo: {
+          ...state.signUpField.addressinfo,
+          zonecode,
+          address,
+        },
+      },
+    }),
   },
 });
 
 export const {
   setTest,
+  setIdForSigningUp,
+  setPasswordForSigingUp,
+  setPhoneAuthChecked,
+  setAddressinfo,
 } = actions;
 
 export default reducer;
