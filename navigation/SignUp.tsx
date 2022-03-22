da232b16822290ef7bc198cd725e4f7871228947
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-03-22 22:38:52 +0900

    [구현] SignUp스택에서 스크린 경로 설정 Auth - SignUp 스크린 제거 Home - eslint

--- navigation/SignUp.tsx
+++ navigation/SignUp.tsx
@@ -7,6 +7,8 @@ import PhoneNumber from './SignUpStack/PhoneNumber';
 import Address from './SignUpStack/Address';
 import AccountNumber from './SignUpStack/AccountNumber';
 import Success from './SignUpStack/Success';
+import PhoneAuth from './SignUpStack/PhoneAuth';
+import SearchAddress from './SignUpStack/SearchAddress';
 
 export default function SignUp() {
   const Stack = createNativeStackNavigator();
@@ -14,22 +16,47 @@ export default function SignUp() {
   return (
     <Stack.Navigator
       initialRouteName="IDPW"
+      screenOptions={{
+        // headerShown:false,
+        // headerTitle:'회원가입',
+        headerTransparent: true,
+      }}
     >
       <Stack.Screen
         name="IDPW"
         component={IDPW}
+        options={{
+          headerTitle: '회원가입',
+        }}
       />
       <Stack.Screen
         name="PhoneNumber"
         component={PhoneNumber}
+        options={{
+          headerTitle: '휴대폰 번호 인증',
+        }}
+      />
+      <Stack.Screen
+        name="PhoneAuth"
+        component={PhoneAuth}
       />
       <Stack.Screen
         name="Address"
         component={Address}
+        options={{
+          headerTitle: '주소',
+        }}
+      />
+      <Stack.Screen
+        name="SearchAddress"
+        component={SearchAddress}
       />
       <Stack.Screen
         name="AccountNumber"
         component={AccountNumber}
+        options={{
+          headerTitle: '계좌번호',
+        }}
       />
       <Stack.Screen
         name="Success"
