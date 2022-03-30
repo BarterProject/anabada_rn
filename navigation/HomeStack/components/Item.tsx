8c1d01d405c690d0c66f06d9f706cf78fc119c3e
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-03-31 01:38:35 +0900

    [chore] ButtonFit에 export추가, Item 스크린 구현

---
+++ navigation/HomeStack/components/Item.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+    flex: 1;
+    justify-content:center;
+    align-items:center;
+`;
+
+const Text = styled.Text`
+    flex: 1;
+    justify-content:center;
+    align-items:center;
+`;
+
+export default function Item() {
+  return (
+    <Container>
+      <Text>
+        Item
+      </Text>
+    </Container>
+  );
+}
