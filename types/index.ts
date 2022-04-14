4a0c0da213aaa50164e8dad2507e5d0ad556ecb2
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-04-14 11:33:58 +0900

    [구현] Item타입과 그 하위 타입 구현 ItemImage, User

--- types/index.ts
+++ types/index.ts
@@ -14,3 +14,46 @@ export type testItem={
     imgName:string;
     name:string;
 }
+
+export type ItemImage={
+    idx: number;
+    name: string;
+    createdAt: string;
+    number: number;
+    originalName: string;
+    saveName: string;
+    size: number;
+    uploadPath: string;
+    extension: string;
+}
+
+export type User = {
+    idx: null;
+    email: string;
+    password: string;
+    phone: string;
+    address:string;
+    bankAccount: string;
+    bankKind: string;
+    createdAt: string;
+    oauth: string;
+    activated: true;
+    auth: string;
+}
+
+export type Item={
+    idx:number;
+    name:string;
+    description:string;
+    createdAt:string;
+    end:string|null;
+    deposit:number;
+    clause_agree:boolean;
+    payment:string|null;
+    itemCategory:any;
+    images : ItemImage[];
+    registrant:User;
+    owner:User;
+    state:number;
+    delivery:null;
+}
