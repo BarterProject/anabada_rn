de999e8d2802006454427b995c9d2091094bc8a5
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-05-04 23:42:57 +0900

    [테스트중] AuthStack/Main

---
+++ navigation/AuthStack/Main.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Main from './Main';
+
+test('renders correctly', () => {
+  expect(1 + 1).toBe(2);
+//   const tree = renderer.create(<Main />).toJSON();
+//   console.log(tree);
+//   expect(tree).toMatchSnapshot();
+});
